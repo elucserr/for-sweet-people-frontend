b851ccb3f6d64384cf211689f512cabcf8efefaa
elucserr <elenalucasserrano@gmail.com>
2020-05-11 18:26:52 +0200

    ordered by views and components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,11 @@
 export { default as PrivateRoute } from "./PrivateRoute";
 export { default as AnonRoute } from "./AnonRoute";
+export { default as Button } from "./Button";
+export { default as ButtonRadius } from "./ButtonRadius";
+export { default as Options } from "./Options";
+export { default as SmallCard } from "./SmallCard";
+export { default as Card } from "./Card";
+export { default as Nav } from "./Nav";
+export { default as Footer } from "./Footer";
+export { default as Input } from "./Input";
+
--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import apiClient from "../services/apiClient";
 
 export default class Signup extends Component {
     render() {
