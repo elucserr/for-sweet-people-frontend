d665f3a97d362af0d9668235f217f0222a6b2da1
elucserr <elenalucasserrano@gmail.com>
2020-05-16 10:59:29 +0200

    view home

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,34 +1,40 @@
 import React, { Component } from "react";
 import apiClient from "../../services/apiClient";
+import Card from "../../components/Card"
 
 export default class Home extends Component {
   state = {
     options: [],
-  };
-
-  loadOptions = () => {
-    apiClient
-    .getOptions()
-    .then(({ data }) => {
-      this.setState({
-        options: data,
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  };
-  componentDidMount(){
-    this.loadOptions();
-  };
-
-  renderOptions = () => {
-
-  };
-
-  render(){
-    return(
-      <div></div>
-    );
   }
+
+  componentDidMount = () => {
+    apiClient
+    .options()
+    .then((response) => {
+      this.setState({
+        options: response.data,
+      })
+    })
+
+  .catch( (error) => {
+    this.setState({
+      error: error.name,
+    })
+  })
 }
+
+showOptions = () => {
+  const { options } = this.state;
+  return options.map((option, index) => {
+    return <Card option={option} index={index}/>
+  })
+}
+
+render(){
+  return(
+    <div>Options</div>
+  )
+ }
+}
+
+  
