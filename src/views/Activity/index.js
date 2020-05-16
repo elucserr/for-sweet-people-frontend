9316983252b178a56397f733e6121c83cff812d5
elucserr <elenalucasserrano@gmail.com>
2020-05-16 09:55:01 +0200

    views with axios

--- src/views/Activity/index.js
+++ src/views/Activity/index.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
-export default class Activity extends Component {
-    render() {
-      return (
-        <div>
-          <h1>Activity</h1>
-        </div>
-      );
+class Activity extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      type: '',
+      km: '',
+      time: ''
     }
   }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+    .post("https://api-forsweetpeople.herokuapp.com/activity", this.state)
+    .then(response => {
+    })
+    .catch((error) => {
+      this.setState({
+        error: error,
+      })
+    })
+
+    this.setState({
+      type: '',
+      km: '',
+      time: ''
+    });
+  }
+
+  render() {
+    const { type, km, time } = this.state;
+    return (
+      <div>Activity</div>
+    )
+  }
+}
+
+export default Activity;
--- src/views/Blood/index.js
+++ src/views/Blood/index.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
-export default class Blood extends Component {
-    render() {
-      return (
-        <div>
-          <h1>Blood</h1>
-        </div>
-      );
+class Blood extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      date: '',
+      time: '',
+      level: ''
     }
   }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+    .post("https://api-forsweetpeople.herokuapp.com/blood", this.state)
+    .then(response => {
+    })
+    .catch((error) => {
+      this.setState({
+        error: error,
+      })
+    })
+
+    this.setState({
+      date: '',
+      time: '',
+      level: ''
+    });
+  }
+
+  render() {
+    const { date, time, level } = this.state;
+    return (
+      <div>blood</div>
+    )
+  }
+}
+
+export default Blood;
