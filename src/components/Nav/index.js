1eedbe0da0d64f2b60d211e8e5a62d47ab632358
elucserr <elenalucasserrano@gmail.com>
2020-05-23 12:14:36 +0200

    menu nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,8 +7,8 @@ class Nav extends Component {
     render() {
         return(
             <nav className="nav">
-                <Link className='nav-link' to='/users/:id'>
-                 <img src="/images/menu.png" alt="m" className="menu"/>
+                <Link className='nav-link' to='/profile'>
+                 <img src="/images/menu.png" alt="menu" className="menu"/>
                 </Link>
                 <Link className='nav-link' to='/options'>F O R S W E E T P E O P L E</Link>
             </nav>
